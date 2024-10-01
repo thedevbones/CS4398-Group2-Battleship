04f4dc376c2f03d45797d63be522a0a1625235db
Shelby Cumings <s_c830@txstate.edu>
2024-10-01 02:02:57 -0500

    Ship.ts basic structure

---
+++ src/Ship.ts
@@ -0,0 +1,36 @@
+class Ship {
+    private length: number;
+    private width: number;
+    private hitArray: number[][];
+    private sunk: boolean;
+
+    constructor(l:number, w:number) {
+        this.length = l;
+        this.width = w;
+        this.hitArray = Array.from({ length: l }, () => Array<number>(w).fill(0));
+        this.sunk = false;
+    }
+
+    hit(x:number, y:number) {
+        this.hitArray[x][y] = 1;
+    }
+
+    isSunk() {
+        let sunkChecker = true;
+
+        // iterate through columns
+        for (let i in this.hit) {
+
+            // iterate through column i's rows
+            for (let j in this.hit[i]) {
+
+                // if any square is not hit, ship is not sunk
+                if (this.hit[i][j] == 0) {
+                    sunkChecker = false;
+                }
+            }
+        }
+        return sunkChecker;
+    }
+
+} export default Ship;
