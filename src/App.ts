c5adfde23ae82be121d85504f413b56a127d5745
devbones <87474953+thedevbones@users.noreply.github.com>
2024-10-03 00:49:05 -0500

    Constructors for ships

--- src/App.ts
+++ src/App.ts
@@ -2,8 +2,13 @@ import * as gfx from 'gophergfx';
 import Ship from './Ship';
 
 export class App extends gfx.GfxApp {
+    private playerShips: Ship[];
+    private aiShips: Ship[];
+
     constructor() {
         super();
+        this.playerShips = [];
+        this.aiShips = [];
     }
 
     createScene(): void {
