25d61bb610c28f9dc7f2a0520ecbd8ac74969fc3
Shelby Cumings <s_c830@txstate.edu>
2024-10-03 12:02:58 -0500

    Opponent algorithm outlines

--- src/EasyOpponent.ts
+++ src/EasyOpponent.ts
@@ -2,6 +2,13 @@ import Opponent from './Opponent'
 
 class EasyOpponent extends Opponent {
 
+    /*
+        1. Choose random until hit
+        2. Choose next space within surrounding 6(+?) spaces
+        3. Continue (2) until sink
+        4. Repeat from (1)
+    */
+
     nextMove(): number[] {
 
         return[0,0]
