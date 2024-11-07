60f4d4d7e6a4d803500c37790023e6e0138bb3f2
devbones <thedevbones@gmail.com>
2024-11-07 10:44:42 -0600

    Added animated ocean grid

--- src/App.ts
+++ src/App.ts
@@ -4,16 +4,27 @@ import Ship from './Ship';
 export class App extends gfx.GfxApp {
     private playerShips: Ship[];
     private aiShips: Ship[];
+    private waterMaterial: gfx.Material2;
+    private waterTextures: gfx.Texture[];
 
     constructor() {
         super();
         this.playerShips = [];
         this.aiShips = [];
+        this.waterMaterial = new gfx.Material2();
+        this.waterTextures = [];
+        for (let i = 1; i < 22; i++) {
+            this.waterTextures.push(new gfx.Texture(`assets/ocean/ocean${i}.png`));
+        }
+        console.log(this.waterTextures);
     }
 
-    createScene(): void {   
+    createScene(): void {       
         // Set background color
         this.renderer.background.set(0.5, 0.5, 0.9, 1);
+        this.waterMaterial.texture = this.waterTextures[0];
+        this.waterMaterial.drawMode = 5
+
         // Define ship lengths
         const shipLengths = [5, 4, 3, 3, 2];
 
@@ -28,9 +39,24 @@ export class App extends gfx.GfxApp {
             const enemyShip = new Ship(length, 1); // Assuming width is always 1
             this.aiShips.push(enemyShip);
         }
+
+        // Create grid of water
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                let waterTile = new gfx.Mesh2();
+                waterTile = gfx.Geometry2Factory.createBox(0.1, 0.1);
+                waterTile.position.set(i * 0.1, j * 0.1);
+                waterTile.material = this.waterMaterial;
+                this.scene.add(waterTile);
+            }
+        }
     }
 
     update(deltaTime: number): void {
+        // Update water
+        const textureIndex = Math.floor((Date.now() / 60) % this.waterTextures.length);
+        this.waterMaterial.texture = this.waterTextures[textureIndex];
+
         // Game logic goes here
     }
 }
